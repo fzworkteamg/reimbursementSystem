0d7f4877421178e0f37a6decc4e7f46109be468f
LINWEIJIE19971223 <1592330717@qq.com>
2019-03-26 08:27:50 +0800

    登陆

--- src/main/java/cn/reimbursement/controller/StaffController.java
+++ src/main/java/cn/reimbursement/controller/StaffController.java
@@ -1,9 +1,12 @@
 package cn.reimbursement.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,10 @@ public class StaffController {
 	private StaffService staffService;
 	
 	@PostMapping("/loginByIdAndPassword")
-	public ServerResult loginByIdAndPassword(HttpServletRequest request,String staffId,String staffPassword) {
+	public ServerResult loginByIdAndPassword(@RequestBody Map<String,String> map,HttpServletRequest request) {
+		String staffId=map.get("staffId");
+		String staffPassword=map.get("staffPassword");
+		//System.out.println(staffId+","+staffPassword+","+request.getParameter("staffId")+","+request.getParameter("staffPassword"));
 		return staffService.loginByIdAndPassword(request, staffId, staffPassword);
 	}
 	
