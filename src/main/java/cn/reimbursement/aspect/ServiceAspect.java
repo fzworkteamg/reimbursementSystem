d87abafda209a4dd22bddc4fd6d4c228378e4134
LINWEIJIE19971223 <1592330717@qq.com>
2019-03-29 14:44:45 +0800

    log4j改 3.29

--- src/main/java/cn/reimbursement/aspect/ServiceAspect.java
+++ src/main/java/cn/reimbursement/aspect/ServiceAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -16,30 +17,39 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Aspect
 @Component
 public class ServiceAspect {
-	
+
 	private Logger logger = Logger.getLogger(ServiceAspect.class);
-	
+
 	@Pointcut("execution(public * cn.reimbursement.service.*.*(..))")
-	public void servicePosition(){}
-	
+	public void servicePosition() {
+	}
+
 	@Before("servicePosition()")
-    public void exBefore(JoinPoint joinPoint){
-        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = sra.getRequest();
-        logger.info("url:"+request.getRequestURL());
-        logger.info("method:"+request.getMethod());
-        logger.info("class method:"+joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName());
-        logger.info("args:"+joinPoint.getArgs().toString());
-    }
-    
-    @After("servicePosition()")
-    public void exAfter(JoinPoint joinPoint){
-        logger.info("class method:"+joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName()
-                +"方法执行完毕！");
-    }
-    
-    @AfterReturning(returning="result",pointcut="servicePosition()")
-    public void exAfterReturning(Object result){
-        logger.info("执行返回值："+result);
-    }
+	public void exBefore(JoinPoint joinPoint) {
+		ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		HttpServletRequest request = sra.getRequest();
+		logger.info("url:" + request.getRequestURL());
+		logger.info("method:" + request.getMethod());
+		logger.info("class method:" + joinPoint.getSignature().getDeclaringTypeName() + "."
+				+ joinPoint.getSignature().getName());
+		Object[] args = joinPoint.getArgs();
+		String[] argNames = ((MethodSignature)joinPoint.getSignature()).getParameterNames();
+		if(args.length==argNames.length) {
+			StringBuilder sb=new StringBuilder("args:\t");
+			for(int i=0;i<args.length;i++) 
+				sb.append(argNames[i]+":"+args[i]+"\t");
+			logger.info(sb);
+		}
+	}
+
+	@After("servicePosition()")
+	public void exAfter(JoinPoint joinPoint) {
+		logger.info("class method:" + joinPoint.getSignature().getDeclaringTypeName() + "."
+				+ joinPoint.getSignature().getName() + "\t compelet!");
+	}
+
+	@AfterReturning(returning = "result", pointcut = "servicePosition()")
+	public void exAfterReturning(Object result) {
+		logger.info("result：" + result);
+	}
 }
