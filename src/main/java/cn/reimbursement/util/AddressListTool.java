a60cae1041abe43e7601acddd6b34210651b0909
flyshuai <914423648@qq.com>
2019-04-12 10:42:01 +0800

    4.12

--- src/main/java/cn/reimbursement/util/AddressListTool.java
+++ src/main/java/cn/reimbursement/util/AddressListTool.java
@@ -2,24 +2,72 @@ package cn.reimbursement.util;
 
 import net.sf.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class AddressListTool {
+
+    static class UrlUtil{
+        static JSONObject getJson(String url) throws IOException {
+             URL urlGet = new URL(url);
+             HttpURLConnection http = (HttpURLConnection) urlGet.openConnection();
+             http.setRequestMethod("GET"); // 必须是get方式请求
+             http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+             http.setDoOutput(true);
+             http.setDoInput(true);
+             System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时30秒
+             System.setProperty("sun.net.client.defaultReadTimeout", "30000"); // 读取超时30秒
+             http.connect();
+             InputStream is = http.getInputStream();
+             int size = is.available();
+             byte[] jsonBytes = new byte[size];
+             is.read(jsonBytes);
+             String message = new String(jsonBytes, "UTF-8");
+             JSONObject demoJson = JSONObject.fromObject(message);
+             System.out.println("JSON字符串：" + demoJson);
+             is.close();
+             return demoJson;
+        }
+    }
+
+    public static List getAllDepartment() throws IOException {
+        String accesstoken2 = AddressListTool.getAccessToken();
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=" + accesstoken2;
+        UrlUtil.getJson(url);
+        return null;
+    }
+
     public static String getAccessToken() {
         String access_token = "";
-//        String grant_type = "client_credential";//获取access_token填写client_credential
-        String AppId= "wxdc2a7022949cf052";//第三方用户唯一凭证
-        String secret= "DDM3QsH6EY-7xanGo1h4PN5ZhVh9Ep6fAxF9elT7qGY";//第三方用户唯一凭证密钥，即appsecret
+        String AppId = "wxdc2a7022949cf052";//第三方用户唯一凭证
+        String secret = "DDM3QsH6EY-7xanGo1h4PN5ZhVh9Ep6fAxF9elT7qGY";//第三方用户唯一凭证密钥，即appsecret
         //这个url链接地址和参数皆不能变
-//        String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type="+grant_type+"&appid="+AppId+"&secret="+secret;
-        String url =  "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid="+AppId+"&corpsecret="+secret;
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + AppId + "&corpsecret=" + secret;
+        try {
+            JSONObject demoJson = UrlUtil.getJson(url);
+            access_token = demoJson.getString("access_token");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(access_token);
+        return access_token;
+    }
+
+    public static String getTicket(String access_token) {
+        String ticket = null;
+        String url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + access_token + "&type=jsapi";//这个url链接和参数不能变
         try {
             URL urlGet = new URL(url);
             HttpURLConnection http = (HttpURLConnection) urlGet.openConnection();
             http.setRequestMethod("GET"); // 必须是get方式请求
-            http.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             http.setDoOutput(true);
             http.setDoInput(true);
             System.setProperty("sun.net.client.defaultConnectTimeout", "30000");// 连接超时30秒
@@ -31,13 +79,35 @@ public class AddressListTool {
             is.read(jsonBytes);
             String message = new String(jsonBytes, "UTF-8");
             JSONObject demoJson = JSONObject.fromObject(message);
-            System.out.println("JSON字符串："+demoJson);
-            access_token = demoJson.getString("access_token");
+            System.out.println("JSON字符串：" + demoJson);
+            ticket = demoJson.getString("ticket");
             is.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(access_token);
-        return access_token;
+        return ticket;
+    }
+
+    public static String SHA1(String decript) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("SHA-1");
+            digest.update(decript.getBytes());
+            byte messageDigest[] = digest.digest();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            // 字节数组转换为 十六进制 数
+            for (int i = 0; i < messageDigest.length; i++) {
+                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexString.append(0);
+                }
+                hexString.append(shaHex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
--- src/test/java/cn/reimbursement/ReimbursementApplicationTests.java
+++ src/test/java/cn/reimbursement/ReimbursementApplicationTests.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.util.UUID;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ReimbursementApplicationTests {
@@ -16,6 +19,40 @@ public class ReimbursementApplicationTests {
 
 	@Test
 	public void test(){
-		AddressListTool.getAccessToken();
+		//1、获取AccessToken
+		String accessToken =AddressListTool.getAccessToken();
+
+		//2、获取Ticket
+		String jsapi_ticket = AddressListTool.getTicket(accessToken);
+
+		//3、时间戳和随机字符串
+		String noncestr = UUID.randomUUID().toString().replace("-", "").substring(0, 16);//随机字符串
+		String timestamp = String.valueOf(System.currentTimeMillis() / 1000);//时间戳
+
+		System.out.println("accessToken:"+accessToken+"\njsapi_ticket:"+jsapi_ticket+"\n时间戳："+timestamp+"\n随机字符串："+noncestr);
+
+		//4、获取url
+		String url="http://www.luiyang.com/add.html";
+    /*根据JSSDK上面的规则进行计算，这里比较简单，我就手动写啦
+    String[] ArrTmp = {"jsapi_ticket","timestamp","nonce","url"};
+    Arrays.sort(ArrTmp);
+    StringBuffer sf = new StringBuffer();
+    for(int i=0;i<ArrTmp.length;i++){
+        sf.append(ArrTmp[i]);
+    }
+    */
+
+		//5、将参数排序并拼接字符串
+		String str = "jsapi_ticket="+jsapi_ticket+"&noncestr="+noncestr+"&timestamp="+timestamp+"&url="+url;
+
+		//6、将字符串进行sha1加密
+		String signature =AddressListTool.SHA1(str);
+		System.out.println("参数："+str+"\n签名："+signature);
+	}
+
+
+	@Test
+	public void test2() throws IOException {
+		AddressListTool.getAllDepartment();
 	}
 }
