a0bbb1665d687a254250b1a3bd9ea4e72020cbe8
LINWEIJIE19971223 <1592330717@qq.com>
2019-04-14 17:18:27 +0800

    4.14

--- src/main/java/cn/reimbursement/dao/BillDao.java
+++ src/main/java/cn/reimbursement/dao/BillDao.java
@@ -10,17 +10,31 @@ import cn.reimbursement.pojo.Bill;
 
 @Repository
 public interface BillDao {
-	List<Bill> selectBillByCompany(@Param("billCompany")String billCompany);
-	Integer selectBillCountById(@Param("billId")String billId);
-	Integer insertBill(@Param("billMap")Map<String,String> billMap);
-	List<Bill> selectBill(@Param("billMap")Map<String,Object> billMap);
-	List<Bill> selectBillByMonth(@Param("year") String year,@Param("month") String month);
-	Bill selectBillById(@Param("billId")String billId);
-	Integer updateBillStatusById(@Param("billId")String billId,@Param("contractStatus")String contractStatus,@Param("invoiceStatus")String invoiceStatus);
-	Integer updateBillEnd(@Param("billId")String billId);
-	Integer selectIsEndById(@Param("billId")String billId);
-	Integer selectRegist(@Param("billId")String billId,@Param("staffCompany")String staffCompany,@Param("staffName")String staffName);
-	Integer updateBill(@Param("billMap")Map<String,String> billMap);
-	Integer deleteBillById(@Param("billId")String billId);
-	Integer selectBillCount(@Param("billMap")Map<String,Object> billMap);
+	List<Bill> selectBillByCompany(@Param("billCompany") String billCompany);
+
+	Integer selectBillCountById(@Param("billId") String billId);
+
+	Integer insertBill(@Param("billMap") Map<String, String> billMap);
+
+	List<Bill> selectBill(@Param("billMap") Map<String, Object> billMap);
+
+	List<Bill> selectBillByMonth(@Param("year") String year, @Param("month") String month);
+
+	Bill selectBillById(@Param("billId") String billId);
+
+	Integer updateBillStatusById(@Param("billId") String billId, @Param("contractStatus") String contractStatus,
+			@Param("invoiceStatus") String invoiceStatus);
+
+	Integer updateBillEnd(@Param("billId") String billId);
+
+	Integer selectIsEndById(@Param("billId") String billId);
+
+	Integer selectRegist(@Param("billId") String billId, @Param("staffCompany") String staffCompany,
+			@Param("staffName") String staffName);
+
+	Integer updateBill(@Param("billMap") Map<String, String> billMap);
+
+	Integer deleteBillById(@Param("billId") String billId);
+
+	Integer selectBillCount(@Param("billMap") Map<String, Object> billMap);
 }
