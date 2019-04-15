6751c1f277200ea67451482b8de35a6b1ebf1b18
flyshuai <914423648@qq.com>
2019-04-15 08:33:59 +0800

    4.14

--- src/main/resources/static/js/addBill.js
+++ src/main/resources/static/js/addBill.js
@@ -164,23 +164,23 @@ function openAddReiPerson() {
         type: 1,
         area: ['600px', '600px'],
         shadeClose: true, //点击遮罩关闭
-        success: function(layero, index){
+        success: function (layero, index) {
             console.log(layero, index);
             var company = $("#company").val();
-            $("#companyInCompanyPane").append("<a style='' href='javascript:;'>"+company+"</a><br>");
-            selectPersonsByCompanyAndDepShowInPane(company,"");
+            $("#companyInCompanyPane").append("<a style='' id='companyInPane' href='javascript:;'>" + company + "</a><br>");
+            selectPersonsByCompanyAndDepShowInPane(company, "", 2, 0);
             $.ajax({
-                url:'/dep/selectDepByCompany',
-                type:'post',
-                dataType:'json',
-                data:{
-                    "company":company
+                url: '/dep/selectDepByCompany',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                    "company": company
                 },
-                async:false,
-                success:function (result) {
+                async: false,
+                success: function (result) {
                     deps = result.data;
-                    for (var i=0;i<deps.length;i++){
-                        $("#depPane").append("<h5 name='dep' style='cursor: pointer' onclick='selectPersonsByCompanyAndDepShowInPane(\""+company+"\",\""+deps[i]+"\")'>"+deps[i]+"</h5><br>");
+                    for (var i = 0; i < deps.length; i++) {
+                        $("#depPane").append("<h5 name='dep' style='cursor: pointer' onclick='selectPersonsByCompanyAndDepShowInPane(\"" + company + "\",\"" + deps[i] + "\",2,1)'>" + deps[i] + "</h5><br>");
                     }
                 }
             });
@@ -227,49 +227,47 @@ function openAddReiPerson() {
         // "</form>\n" +
         // "</div>"
     });
-    var companys = selectCompany();
-    romance(companys, "childSelectCompany");
-    form.render();
 }
 
 function openAddChargePerson() {
     chargePersonIndex = layer.open({
         type: 1,
-        area: ['400px', '300px'],
+        area: ['600px', '600px'],
         shadeClose: true, //点击遮罩关闭
-        content: "<div class='layui-row' style='text-align: center'>\n" +
-            "<form class=\"layui-form\" action=\"\">\n" +
-            "<div class=\"layui-form-item\">\n" +
-            "    <label class=\"layui-form-label\">选择公司：</label>\n" +
-            "    <div class=\"layui-input-block\">\n" +
-            "        <select id='childSelectCompany' name='childSelectCompany1' lay-filter='childSelectCompany' lay-verify=\"required\">\n" +
-            "<option></option>" +
-            "        </select>\n" +
-            "    </div>\n" +
-            "</div>\n" +
-            "<div class=\"layui-form-item\">\n" +
-            "    <label class=\"layui-form-label\">选择部门：</label>\n" +
-            "    <div class=\"layui-input-block\">\n" +
-            "        <select id='childSelectDep' lay-verify=\"required\" lay-filter='childSelectDep'>\n" +
-            "<option></option>" +
-            "        </select>\n" +
-            "    </div>\n" +
-            "</div>\n" +
-            "<div class=\"layui-form-item\">\n" +
-            "    <label class=\"layui-form-label\">选择员工：</label>\n" +
-            "    <div class=\"layui-input-block\">\n" +
-            "        <select name=\"childSelectPerson\" lay-verify=\"required\" lay-filter='childSelectPerson' id='childSelectPerson'>\n" +
-            "<option></option>" +
-            "        </select>\n" +
-            "    </div>\n" +
-            "</div>\n" +
-            "<button type='button' class='layui-btn' lay-filter='chooseChargePerson' onclick='chooseChargePersonAndCloseTip()' id='chooseChargePersonButton'>确定</button>" +
-            "</form>\n" +
+        success: function (layero, index) {
+            console.log(layero, index);
+            var company = $("#company").val();
+            $("#companyInCompanyPane").append("<a style='' id='companyInPane' href='javascript:;'>" + company + "</a><br>");
+            selectPersonsByCompanyAndDepShowInPane(company, "", 3, 0);
+            $.ajax({
+                url: '/dep/selectDepByCompany',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                    "company": company
+                },
+                async: false,
+                success: function (result) {
+                    deps = result.data;
+                    for (var i = 0; i < deps.length; i++) {
+                        $("#depPane").append("<h5 name='dep' style='cursor: pointer' onclick='selectPersonsByCompanyAndDepShowInPane(\"" + company + "\",\"" + deps[i] + "\",3,1)'>" + deps[i] + "</h5><br>");
+                    }
+                }
+            });
+        },
+        content:
+            "<div class='layui-row'>" +
+            "<div style='border-right-style: solid;float: left; width: 180px; text-align: center' id='companyPane'>" +
+            "<h4 id='companyInCompanyPane'></h4>" +
+            "</div> " +
+            "<div style='border-right-style: solid;float: left; width: 180px; text-align: center'id='depPane'>" +
+            "" +
+            "</div> " +
+            "<div id='staffPane' style='border-right-style: solid;float: left; width: 180px; text-align: center'>" +
+            "" +
+            "</div> " +
             "</div>"
     });
-    var companys = selectCompany();
-    romance(companys, "childSelectCompany");
-    form.render();
 }
 
 function romanceTable() {
@@ -292,24 +290,24 @@ function romanceTable() {
 
 //弹出层选择报销人返回
 function chooseReiPersonAndCloseTip() {
-    var data = $("#childSelectPerson").val();
-    var arr = data.split("|");
     $("#reiPerson").attr("readOnly", false);
+    var data = event.target.innerHTML;
+    var arr = data.split("|");
+    console.log(arr);
     $("#reiPerson").val(arr[0]);
-    $("#staffCompany").val($("#childSelectCompany").val());
-    $("#staffDep").val($("#childSelectDep").val());
     layer.close(reiPersonIndex);
+    $("#reiPerson").attr("readOnly", true);
 }
 
 //弹出层选择经办人返回
-function chooseReiStaff() {
-    $("#reiPerson").attr("readOnly", false);
-    var staff  = event.target.innerHTML;
+function chooseChargeAndCloseTip() {
+    $("#chargePerson").attr("readOnly", false);
+    var staff = event.target.innerHTML;
     var temp = staff.split("|");
     console.log(temp[0]);
-    $("#reiPerson").val(temp[0]);
-    layer.close(reiPersonIndex);
-    $("#reiPerson").attr("readOnly", true);
+    $("#chargePerson").val(temp[0]);
+    layer.close(chargePersonIndex);
+    $("#chargePerson").attr("readOnly", true);
 }
 
 //根据时间生成单号
@@ -333,25 +331,52 @@ function billIdByTime() {
 }
 
 //弹出层根据公司部门填充职工栏
-var count=0;
+var count = 0;
 var oldClick;
-function selectPersonsByCompanyAndDepShowInPane(company, dep) {
-    if (count == 0) {
-        oldClick = event.currentTarget;
-        oldClick.style.color = 'red';
-        count++;
-    } else {
-        oldClick.style.color = 'black';
-        var newClick = event.currentTarget;
-        newClick.style.color = 'red';
-        oldClick = newClick;
+
+function selectPersonsByCompanyAndDepShowInPane(company, dep, type, flag) {
+    //type:3代表选择经办人，2代表选择报销人
+    //flag:0代表刚弹出页面直接显示公司的全部职员，1代表根据公司部门筛选员工
+    if (type == 2) {
+        if (flag != 0) {
+            if (count == 0) {
+                oldClick = event.currentTarget;
+                oldClick.style.color = 'red';
+                count++;
+            } else {
+                oldClick.style.color = 'black';
+                var newClick = event.currentTarget;
+                newClick.style.color = 'red';
+                oldClick = newClick;
+            }
+        }
+        var persons = selectPersonsByCompanyAndDep(company, dep);
+        $("#staffPane").empty();
+        for (var i = 0; i < persons.length; i++) {
+            $("#staffPane").append("<h5 onclick='chooseReiPersonAndCloseTip()' style='cursor: pointer'>" + persons[i] + "</h5><br>");
+        }
     }
-    console.log(company,dep);
-    var persons = selectPersonsByCompanyAndDep(company, dep);
-    $("#staffPane").empty();
-    for (var i = 0; i < persons.length; i++) {
-        $("#staffPane").append("<h5 onclick='chooseReiStaff()' style='cursor: pointer'>" + persons[i] + "</h5><br>");
+
+    if (type == 3) {
+        if (flag != 0) {
+            if (count == 0) {
+                oldClick = event.currentTarget;
+                oldClick.style.color = 'red';
+                count++;
+            } else {
+                oldClick.style.color = 'black';
+                var newClick = event.currentTarget;
+                newClick.style.color = 'red';
+                oldClick = newClick;
+            }
+        }
+        var persons = selectPersonsByCompanyAndDep(company, dep);
+        $("#staffPane").empty();
+        for (var i = 0; i < persons.length; i++) {
+            $("#staffPane").append("<h5 onclick='chooseChargeAndCloseTip()' style='cursor: pointer'>" + persons[i] + "</h5><br>");
+        }
     }
+
 }
 
 
